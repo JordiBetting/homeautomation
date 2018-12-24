7d1eb7131808a2afe77921e0382207e19e87cf50
Jordi <jordi.betting@gmail.com>
2018-12-24 17:38:29 +0100

    Use JUnit5 assertions rather than JUnit4 assertions

--- automation_framework/src/test/java/nl/gingerbeard/automation/AutomationFrameworkTest.java
+++ automation_framework/src/test/java/nl/gingerbeard/automation/AutomationFrameworkTest.java
@@ -1,8 +1,8 @@
 package nl.gingerbeard.automation;
 
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
