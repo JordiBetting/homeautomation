bde297ac1c6ba49e3d12fefc21c912f29c702dbb
Jordi <jordi.betting@gmail.com>
2018-12-31 17:01:33 +0100

    Start WhiteAmbiance. Added documentation

--- automation_framework/src/main/java/nl/gingerbeard/automation/devices/ColorLight.java
+++ automation_framework/src/main/java/nl/gingerbeard/automation/devices/ColorLight.java
@@ -1,9 +1,15 @@
 package nl.gingerbeard.automation.devices;
 
-public class ColorLight extends WhiteAmbianceLight {
+public class ColorLight extends Device<Color> {
 
 	public ColorLight(final int idx) {
 		super(idx);
+	}
+
+	@Override
+	public boolean updateState(final String newState) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
---
+++ automation_framework/src/main/java/nl/gingerbeard/automation/state/Color.java
@@ -0,0 +1,5 @@
+package nl.gingerbeard.automation.state;
+
+public class Color {
+
+}
