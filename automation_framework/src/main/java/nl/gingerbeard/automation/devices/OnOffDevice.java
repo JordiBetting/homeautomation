b157fcdeb5c4a61c59ab36d4c24b19ff6967f961
Jordi <jordi.betting@gmail.com>
2018-12-31 16:36:58 +0100

    Fix locale in OnOffDevice to be consistent with the rest of the framework

--- automation_framework/src/main/java/nl/gingerbeard/automation/devices/OnOffDevice.java
+++ automation_framework/src/main/java/nl/gingerbeard/automation/devices/OnOffDevice.java
@@ -13,7 +13,7 @@ public abstract class OnOffDevice extends Device<OnOffState> {
 	@Override
 	public boolean updateState(final String newStateString) {
 		try {
-			final OnOffState newState = OnOffState.valueOf(newStateString.toUpperCase(Locale.ENGLISH));
+			final OnOffState newState = OnOffState.valueOf(newStateString.toUpperCase(Locale.US));
 			setState(newState);
 			return true;
 		} catch (final IllegalArgumentException e) {
