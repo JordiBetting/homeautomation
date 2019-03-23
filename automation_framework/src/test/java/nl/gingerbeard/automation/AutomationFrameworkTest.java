be917a642a853315bf794efeedccd6784038ffc4
Jordi <jordi.betting@gmail.com>
2019-03-23 06:59:26 +0100

    Forgot to make integration test synchronous, resulting in flaky test

--- automation_framework/src/test/java/nl/gingerbeard/automation/AutomationFrameworkTest.java
+++ automation_framework/src/test/java/nl/gingerbeard/automation/AutomationFrameworkTest.java
@@ -32,6 +32,7 @@ import nl.gingerbeard.automation.devices.Switch;
 import nl.gingerbeard.automation.devices.Thermostat;
 import nl.gingerbeard.automation.devices.ThermostatModeDevice;
 import nl.gingerbeard.automation.devices.ThermostatSetpointDevice;
+import nl.gingerbeard.automation.domoticz.DomoticzThreadHandler;
 import nl.gingerbeard.automation.domoticz.configuration.DomoticzConfiguration;
 import nl.gingerbeard.automation.domoticz.receiver.IDomoticzEventReceiver;
 import nl.gingerbeard.automation.event.IEvents;
@@ -94,6 +95,10 @@ public class AutomationFrameworkTest {
 
 		final Optional<IAutomationFrameworkInterface> framework = container.getRuntime().getService(IAutomationFrameworkInterface.class);
 		assertTrue(framework.isPresent());
+
+		final Optional<DomoticzThreadHandler> threadHandler = container.getRuntime().getService(DomoticzThreadHandler.class);
+		assertTrue(threadHandler.isPresent());
+		threadHandler.get().setSynchronous();
 
 		return framework.get();
 	}
