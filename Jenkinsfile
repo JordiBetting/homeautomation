05e3937b97ef3151a5e510faec1093c85b5fbec7
Jordi Bettig <jordibetting@gmail.com>
2019-10-01 10:39:41 +0200

    Add first version of buildscript

---
+++ Jenkinsfile
@@ -0,0 +1,24 @@
+pipeline {
+	agent { 
+		label 'docker'
+	}
+
+	stages {
+		stage("Prep") {
+			steps {
+				sh 'ln -s build.sh publish.sh'
+			}
+		}
+		stage("Build") {
+			steps {
+				sh './build.sh'
+			}	
+		}
+
+		stage("Publish") {
+			steps {
+				sh './publish.sh'
+			}
+		}
+	}
+}
---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTNAME=`basename "$0"`
+SCRIPTNAME=${SCRIPTNAME%.*}
+
+WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Create version
+COMMITCOUNT=$(git -C ${WORKSPACE} rev-list --count)
+BRANCH=$(git -C ${WORKSPACE} rev-parse --abbrev-ref HEAD)
+VERSION="${COMMITCOUNT}-${BRANCH}"
+
+TAG="jordibetting/jordibetting:java8build-${VERSION}"
+
+if [ "build" == $SCRIPTNAME ]; then
+	docker build -t $TAG ${WORKSPACE}
+elif [ "publish" == $SCRIPTNAME ]; then
+    docker push $TAG
+else
+	>&2 echo "Unsupported action '${SCRIPTNAME}'"
+	exit 1
+fi
