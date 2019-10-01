7dbb47735e82aa46b1009657d5a282d4de27bbfc
Jordi Bettig <jordibetting@gmail.com>
2019-10-01 10:47:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,16 +7,17 @@ SCRIPTNAME=`basename "$0"`
 SCRIPTNAME=${SCRIPTNAME%.*}
 
 WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $WORKSPACE
 
 # Create version
-COMMITCOUNT=$(git -C ${WORKSPACE} rev-list --count)
-BRANCH=$(git -C ${WORKSPACE} rev-parse --abbrev-ref HEAD)
+COMMITCOUNT=$(git rev-list --count)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 VERSION="${COMMITCOUNT}-${BRANCH}"
 
 TAG="jordibetting/jordibetting:java8build-${VERSION}"
 
 if [ "build" == $SCRIPTNAME ]; then
-	docker build -t $TAG ${WORKSPACE}
+	docker build -t $TAG .
 elif [ "publish" == $SCRIPTNAME ]; then
     docker push $TAG
 else
