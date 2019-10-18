e6006cfa14d283fc9db6b4dff573970de9048db5
Jordi Betting <jordibetting@gmail.com>
2019-10-18 22:59:39 +0200

    Yeah, forgot some important stuff

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -9,6 +9,6 @@ TMP_DIR=$(mktemp -d -t buildDockerGingerbeard-XXXXXXXXXX)
 find ./ -name 'automation_framework-*.jar' -exec cp {} . \;
 find ./ -name 'automation_autocontrol-*.jar' -exec cp {} . \;
 
-docker build --build-arg -t gingerbeard-automation-$1 .
+docker build -t gingerbeard-automation-$1 .
 
 rm -rf $TMP_DIR
