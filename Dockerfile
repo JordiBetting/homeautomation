061f27ff3f762787bb3a6d34ecdfde878c76ce7b
Jordi Betting <jordibetting@gmail.com>
2019-10-19 23:51:27 +0200

    Update script to have parameters instead of hardcoded numbers

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,16 @@
 FROM centos:8
 
+ARG username
+ARG userid
+ARG groupname
+ARG groupid
+
 RUN yum install -y \
        java-1.8.0-openjdk \
        java-1.8.0-openjdk-devel \
        git \
 	   which && \
     yum clean all \
-    groupadd -g 989 docker && \
-	useradd -u 1001 -g 989 jenkins
-// TODO: hardcoded group and user id for jenkins and docker should be smarter
+    groupadd -g ${groupid} ${groupname} && \
+	useradd -u ${userid} -g ${groupid} ${username}
 	
--- build.sh
+++ build.sh
@@ -6,13 +6,25 @@ set -x # print all commands
 SCRIPTNAME=`basename "$0"`
 SCRIPTNAME=${SCRIPTNAME%.*}
 
+USERNAME=$USER
+GROUPNAME=docker
+USERID=$(id -u)
+GROUPID=$(getent group docker | awk -F: '{printf "%d\n", $3}')
+
+
 WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 COMMITCOUNT=$(git -C ${WORKSPACE} rev-list --count HEAD)
 TAG="jordibetting/jordibetting:java8build-${COMMITCOUNT}"
 
 if [ "build" == $SCRIPTNAME ]; then
-	docker build -t $TAG ${WORKSPACE}
+	docker build \
+	-t $TAG \
+	--build-arg userid=${USERID} \
+	--build-arg username=${USERNAME} \
+	--build-arg groupname=${GROUPNAME} \
+	--build-arg groupid=${GROUPID} \
+	${WORKSPACE}
 elif [ "publish" == $SCRIPTNAME ]; then
     docker push $TAG
 else
