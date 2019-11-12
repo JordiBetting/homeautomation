569e58c61587c6bb707a41f62ee77f970f2e9f11
JordiBetting <jordi.betting@gmail.com>
2019-11-12 18:32:25 +0100

    Thermostat: Add support for multiple setpoints  (#72)

--- devices/src/test/java/nl/gingerbeard/automation/devices/ThermostatTest.java
+++ devices/src/test/java/nl/gingerbeard/automation/devices/ThermostatTest.java
@@ -1,6 +1,7 @@
 package nl.gingerbeard.automation.devices;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -17,7 +18,7 @@ public class ThermostatTest {
 
 	@Test
 	public void updateMode_parentUpdated() {
-		final Thermostat thermostat = new Thermostat(1, 2);
+		final Thermostat thermostat = new Thermostat(2, 1);
 
 		thermostat.getModeDevice().updateState("SETPOINT");
 
@@ -26,9 +27,9 @@ public class ThermostatTest {
 
 	@Test
 	public void updateSetpoint_parentUpdated() {
-		final Thermostat thermostat = new Thermostat(1, 2);
+		final Thermostat thermostat = new Thermostat(2, 1);
 
-		thermostat.getSetpointDevice().updateState("-14");
+		thermostat.getSetpointDevice().get(0).updateState("-14");
 
 		assertTrue(thermostat.getState().getSetPoint().isPresent());
 		assertEquals(-14, thermostat.getState().getSetPoint().get().get(Unit.CELSIUS));
@@ -36,18 +37,18 @@ public class ThermostatTest {
 
 	@Test
 	public void thermostatSubdevices_parentSet() {
-		final Thermostat thermostat = new Thermostat(1, 2);
+		final Thermostat thermostat = new Thermostat(2, 1);
 
 		assertTrue(thermostat.getModeDevice().getParent().isPresent());
 		assertEquals(thermostat, thermostat.getModeDevice().getParent().get());
 
-		assertTrue(thermostat.getSetpointDevice().getParent().isPresent());
-		assertEquals(thermostat, thermostat.getSetpointDevice().getParent().get());
+		assertTrue(thermostat.getSetpointDevice().get(0).getParent().isPresent());
+		assertEquals(thermostat, thermostat.getSetpointDevice().get(0).getParent().get());
 	}
 
 	@Test
 	public void thermostat_createNextState_off() {
-		final Thermostat thermostat = new Thermostat(1, 2);
+		final Thermostat thermostat = new Thermostat(2, 1);
 		final ThermostatState state = new ThermostatState();
 		state.setOff();
 
@@ -60,7 +61,7 @@ public class ThermostatTest {
 
 	@Test
 	public void thermostat_createNextState_fullheat() {
-		final Thermostat thermostat = new Thermostat(1, 2);
+		final Thermostat thermostat = new Thermostat(2, 1);
 		final ThermostatState state = new ThermostatState();
 		state.setFullHeat();
 
@@ -73,7 +74,7 @@ public class ThermostatTest {
 
 	@Test
 	public void thermostat_createNextState_setpoint() {
-		final Thermostat thermostat = new Thermostat(1, 2);
+		final Thermostat thermostat = new Thermostat(2, 1);
 		final ThermostatState state = new ThermostatState();
 		state.setTemperature(Temperature.celcius(1));
 
@@ -85,5 +86,40 @@ public class ThermostatTest {
 		assertEquals(ThermostatSetpointDevice.class, nextState.get(1).getDevice().getClass());
 		assertEquals(Temperature.class, nextState.get(1).get().getClass());
 		assertEquals(1, ((Temperature) nextState.get(1).get()).get(Unit.CELSIUS));
+	}
+	
+	@Test
+	public void thermostat_mutlipleSetpoints_equal() {
+		final Thermostat thermostat = new Thermostat(1, 2, 3);
+		final ThermostatState state = new ThermostatState();
+		state.setTemperature(Temperature.celcius(1));
+
+		final List<NextState<?>> nextState = thermostat.createNextState(state);
+
+		assertEquals(3, nextState.size());
+		
+		assertEquals(ThermostatModeDevice.class, nextState.get(0).getDevice().getClass());
+		assertEquals(ThermostatMode.SETPOINT, nextState.get(0).get());
+		
+		assertEquals(ThermostatSetpointDevice.class, nextState.get(1).getDevice().getClass());
+		assertEquals(Temperature.class, nextState.get(1).get().getClass());
+		assertEquals(1, ((Temperature) nextState.get(1).get()).get(Unit.CELSIUS));
+
+		assertEquals(ThermostatSetpointDevice.class, nextState.get(2).getDevice().getClass());
+		assertEquals(Temperature.class, nextState.get(2).get().getClass());
+		assertEquals(1, ((Temperature) nextState.get(2).get()).get(Unit.CELSIUS));
+	}
+	
+	@Test
+	public void thermostat_nullList_throwsException() {
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Thermostat(1, null));
+		assertEquals("idxSetpointDevices should not be null", e.getMessage());
+	}
+	
+	@Test
+	public void thermostat_emptyList_throwsException() {
+		int[] empty = {};
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Thermostat(1, empty));
+		assertEquals("idxSetPointDevices array should not be empty", e.getMessage());
 	}
 }
