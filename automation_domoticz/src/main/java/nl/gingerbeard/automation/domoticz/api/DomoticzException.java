c3c5fda2716d51ab849713b79abeed1f0a0d5b06
Jordi <jordi.betting@gmail.com>
2019-12-24 15:48:11 +0100

    Remove obsolete constructor of DomoticzException

--- automation_domoticz/src/main/java/nl/gingerbeard/automation/domoticz/api/DomoticzException.java
+++ automation_domoticz/src/main/java/nl/gingerbeard/automation/domoticz/api/DomoticzException.java
@@ -4,10 +4,6 @@ public class DomoticzException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
-	public DomoticzException() {
-		super();
-	}
-	
 	public DomoticzException(String message, Throwable cause) {
 		super(message, cause);
 	}
